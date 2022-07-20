4bc390905692990f55e6c1dd7048d6912026e63a
masterkai <kai0919@hotmail.com>
2022-07-20 17:58:25 +0800

    second commit

--- src/App.jsx
+++ src/App.jsx
@@ -126,14 +126,74 @@ function App() {
   }, []);
   return (
     <div>
-      <p className="text-center fixed top-0 bottom-0 left-0 right-0 flex justify-center items-center">
-        <button
-          onClick={() => console.log(state)}
-          className="text-white bg-gradient-to-b from-indigo-500 to-indigo-600 px-4 py-3 rounded text-2xl font-bold"
-        >
-          Play
-        </button>
-      </p>
+      {state.currentQuestion && (
+        <>
+          <p className="text-center">
+            <span className="text-zinc-400 mr-3">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="32"
+                height="32"
+                fill="currentColor"
+                className={
+                  "inline-block " + (state.playing ? "animate-spin" : "")
+                }
+                viewBox="0 0 16 16"
+              >
+                <path d="M8.515 1.019A7 7 0 0 0 8 1V0a8 8 0 0 1 .589.022l-.074.997zm2.004.45a7.003 7.003 0 0 0-.985-.299l.219-.976c.383.086.76.2 1.126.342l-.36.933zm1.37.71a7.01 7.01 0 0 0-.439-.27l.493-.87a8.025 8.025 0 0 1 .979.654l-.615.789a6.996 6.996 0 0 0-.418-.302zm1.834 1.79a6.99 6.99 0 0 0-.653-.796l.724-.69c.27.285.52.59.747.91l-.818.576zm.744 1.352a7.08 7.08 0 0 0-.214-.468l.893-.45a7.976 7.976 0 0 1 .45 1.088l-.95.313a7.023 7.023 0 0 0-.179-.483zm.53 2.507a6.991 6.991 0 0 0-.1-1.025l.985-.17c.067.386.106.778.116 1.17l-1 .025zm-.131 1.538c.033-.17.06-.339.081-.51l.993.123a7.957 7.957 0 0 1-.23 1.155l-.964-.267c.046-.165.086-.332.12-.501zm-.952 2.379c.184-.29.346-.594.486-.908l.914.405c-.16.36-.345.706-.555 1.038l-.845-.535zm-.964 1.205c.122-.122.239-.248.35-.378l.758.653a8.073 8.073 0 0 1-.401.432l-.707-.707z" />
+                <path d="M8 1a7 7 0 1 0 4.95 11.95l.707.707A8.001 8.001 0 1 1 8 0v1z" />
+                <path d="M7.5 3a.5.5 0 0 1 .5.5v5.21l3.248 1.856a.5.5 0 0 1-.496.868l-3.5-2A.5.5 0 0 1 7 9V3.5a.5.5 0 0 1 .5-.5z" />
+              </svg>
+              <span className="font-mono text-4xl relative top-2 ml-3">
+                0:
+                {state.timeRemaining < 10
+                  ? "0" + state.timeRemaining
+                  : state.timeRemaining}
+              </span>
+            </span>
+
+            {[...Array(3 - state.strikes)].map((item, index) => {
+              return (
+                <HeartIcon key={index} className="inline text-pink-600 mx-1" />
+              );
+            })}
+            {[...Array(state.strikes)].map((item, index) => {
+              return (
+                <HeartIcon key={index} className="inline text-pink-100 mx-1" />
+              );
+            })}
+          </p>
+          <h1 className="text-center font-bold pt-3 pb-10 break-all text-4xl md:text-7xl">
+            {state.currentQuestion.breed}
+          </h1>
+          <div className="grid grid-cols-2 lg:grid-cols-4 gap-5 px-5">
+            {state.currentQuestion.photos.map((photo, index) => {
+              return (
+                <div
+                  onClick={() =>
+                    dispatch({ type: "guessAttempt", value: index })
+                  }
+                  key={index}
+                  className="rounded-lg h-40 lg:h-80 bg-cover bg-center cursor-pointer"
+                  style={{ backgroundImage: `url(${photo})` }}
+                ></div>
+              );
+            })}
+          </div>
+        </>
+      )}
+      {state.playing === false &&
+        Boolean(state.bigCollection.length) &&
+        !state.currentQuestion && (
+          <p className="text-center fixed top-0 bottom-0 left-0 right-0 flex justify-center items-center">
+            <button
+              onClick={() => dispatch({ type: "startPlaying" })}
+              className="text-white bg-gradient-to-b from-indigo-500 to-indigo-600 px-4 py-3 rounded text-2xl font-bold"
+            >
+              Play
+            </button>
+          </p>
+        )}
     </div>
   );
 }
